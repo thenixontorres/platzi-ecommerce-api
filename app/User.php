e8f1f5b45d248e292ffa4c5f6ff3690204874bb7
thenixontorres <nixontorres2@gmail.com>
2020-07-28 22:55:21 -0400

    class 8

--- app/User.php
+++ app/User.php
@@ -2,16 +2,16 @@
 
 namespace App;
 
+use App\Utils\CanBeRate;
 use App\Utils\CanRate;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable, HasApiTokens, CanRate;
+    use Notifiable, HasApiTokens, CanRate, CanBeRate;
 
     /**
      * The attributes that are mass assignable.
